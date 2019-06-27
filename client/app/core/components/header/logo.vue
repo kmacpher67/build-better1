9e0233c7795a71ea72be41dc6204a8dbe810e3a5
kenmac <ken.macpherson@philips.com>
2019-06-27 09:52:11 -0400

    init building better based on docker vuejs nodejs framework

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span 
+				strong Warren
+</template>
+
+<script>
+
+	export default {
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
---
+++ client/app/modules/home/index.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+	.container
+		h1 Building a Better Warren vc0 
+
+				fieldset
+					legend Helpful Information
+					.content.flex.align-center.justify-space-around.panels
+						.card
+							img.img(src="http://lorempixel.com/350/150/city")
+							.block 
+								.title Water Bill Info
+								p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dicta cumque assumenda culpa maiores omnis totam obcaecati voluptas eligendi sapiente enim debitis, illo dolore facilis, in, nihil perspiciatis, et perferendis.
+							.block
+								button.button.success Water Bill
+								small.text-muted Last updated 5 days ago
+
+						.card
+							.ribbon.right.orange 
+								span -20%
+							img.img(src="http://lorempixel.com/350/150/city")
+							.block 
+								.title Pot Hole Complaints
+								p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dicta cumque assumenda culpa maiores omnis totam obcaecati voluptas eligendi sapiente enim debitis, illo dolore facilis, in, nihil perspiciatis, et perferendis.
+							.block
+								button.button.success Potholes
+
+						.card
+							.ribbon.left.primary 
+								span Brand new
+							img.img(src="http://placehold.it/350x150")
+							.block 
+								.title Card Title wih ribbon
+								p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dicta cumque assumenda culpa maiores omnis totam obcaecati voluptas eligendi sapiente enim debitis, illo dolore facilis, in, nihil perspiciatis, et perferendis.
+							.block.text-right
+								button.button.danger DELETE
+								button.button.outline Close
+														
+</template>
